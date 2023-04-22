3ddc5339f84f023e3d27c9d9199b78ef47a8d6cc
Ashmika-Gujarathi-Globalstep <61505521+Ashmika-Gujarathi-Globalstep@users.noreply.github.com>
2023-04-22 20:06:07 +0530

    Initial Setup

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CIDemoGitHUbAction"
+include(":androidApp")
+include(":shared")
